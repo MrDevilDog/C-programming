f2e8ba00ec3fb883b683848d20c697da75a8d399
MrDevilDog <harshitmishra2258@eng.rizvi.edu.in>
2024-11-17 09:40:33 +0530

    Add files via upload

---
+++ Experiment5/Exp5task1.c
@@ -0,0 +1,33 @@
+/*Aim-WAP to find the factorial of a number
+ using iterative function */
+
+/*Name=Harshit Mishra
+UIN=241A050
+Roll no=41
+Div=A
+Branch=AIDS*/
+#include<stdio.h>
+int FactorialIterative(int num){
+int prod = 1;
+for (int i = 1; i <=num; i++){
+    prod *=i;
+
+}
+return prod;
+}
+int main (){
+int num ;
+printf("\t\t\t*** Iterative Factorial Function *** \n\n");
+printf("Enter Number :");
+scanf("%d",&num);// Take the value from user
+// Calculate the factorial
+printf("Factorial of %d is %d ",num , FactorialIterative (num));
+// Print the result
+return 0 ;
+}
+/*OUTPUT    *** Iterative Factorial Function ***
+
+Enter Number :6
+Factorial of 6 is 720
+Process returned 0 (0x0)   execution time : 5.468 s
+Press any key to continue.*/
